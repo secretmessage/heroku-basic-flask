975d50d7b1a965e2f73e67fbd6b6b8d3ffacc5ca
secretmessage <secretmessage@protonmail.com>
2019-09-14 06:35:33 -0600

    add python version number

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask
+import sys
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    
+    version_string = sys.version
 
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
+    
+    <p>
+        I am on {version-string}.
+    </p>
 
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
